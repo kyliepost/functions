c2a388133e06fd0105827116af7ce23488bfe914
kyliepost <kylie.postlethwaite00@gmail.com>
2021-07-20 13:36:38 -0500

    first commit; working through the explore chapters in chapter 2

---
+++ practice.js
@@ -0,0 +1,38 @@
+
+// const calculateAgeInDogYears = (ageOfPerson) => {
+//     const ageInDogYears = ageOfPerson / 7
+//     // console.log(ageInDogYears)
+
+// return ageInDogYears
+// }
+
+// const dogAge = calculateAgeInDogYears()
+// console.log(dogAge)
+
+
+// const createAFunction = (dogBreed) => {
+//     let whatWeReturn = ""
+
+//     if(dogBreed === "shnauzer"){
+//     whatWeReturn = `My favorite dog breed is ${dogBreed}`
+//     } else if (dogBreed === "meow") {
+//         whatWeReturn = "I like cats"
+//     } else {
+//         whatWeReturn = `${dogBreed} are good dogs`
+//     }
+//     return whatWeReturn
+// }
+
+// const returnValue = createAFunction("shnauzer")
+// const anotherValue = createAFunction("meow")
+// const thirdValue = createAFunction("labrador")
+
+// console.log(returnValue)
+// console.log(anotherValue)
+// console.log(thirdValue)
+
+
+
+const add = (a, b) => {
+    
+}
