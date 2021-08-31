431f986ef946645b7864646b45e20113b7f57b2e
kyliepost <kylie.postlethwaite00@gmail.com>
2021-08-31 14:55:46 -0500

    working on practice 5

--- practice.js
+++ practice.js
@@ -1,38 +1,105 @@
-
+// initial example
 // const calculateAgeInDogYears = (ageOfPerson) => {
 //     const ageInDogYears = ageOfPerson / 7
-//     // console.log(ageInDogYears)
-
 // return ageInDogYears
 // }
 
 // const dogAge = calculateAgeInDogYears()
 // console.log(dogAge)
 
-
+// best in show practice
 // const createAFunction = (dogBreed) => {
 //     let whatWeReturn = ""
-
-//     if(dogBreed === "shnauzer"){
-//     whatWeReturn = `My favorite dog breed is ${dogBreed}`
+//     if (dogBreed === "shnauzer") {
+//     whatWeReturn = `my favorite dog breed is ${dogBreed}` 
 //     } else if (dogBreed === "meow") {
 //         whatWeReturn = "I like cats"
-//     } else {
-//         whatWeReturn = `${dogBreed} are good dogs`
 //     }
-//     return whatWeReturn
+//      return whatWeReturn
 // }
 
-// const returnValue = createAFunction("shnauzer")
-// const anotherValue = createAFunction("meow")
-// const thirdValue = createAFunction("labrador")
+// const firstValue = createAFunction("shnauzer")
+// const secondValue = createAFunction("meow")
+// console.log(secondValue)
 
-// console.log(returnValue)
-// console.log(anotherValue)
-// console.log(thirdValue)
-
-
-
-const add = (a, b) => {
+// addition
+// const add = (a, b, c) => {
+//     return a + b + c
     
+// }
+
+// const sum = add(17, 4, 11)
+// const sum2 = add(5, 2)
+// const sum3 = add(3, 3)
+// console.log(sum)
+// console.log(sum2)
+// console.log(sum3)
+
+// self-driving cars
+// const go = (direction, speed) => {
+//    if (speed <= 75) {
+
+//     return `the car is moving ${direction} at ${speed} mph.`
+//    } else {
+//        return `SLOW DOWN`
+//    }
+    
+// }
+
+// const string = go(`forwards`, 45)
+// const string2 = go(`backwards`, 8)
+// const string3 = go(`in circles`, 12)
+// const string4 = go(`forwards`, 82)
+// console.log(string)
+// console.log(string2)
+// console.log(string3)
+// console.log(string4)
+
+
+// evens or odds
+// const evenOrOdd = (number) => {
+//    if (number % 2 === 0) {
+//     return `even`
+// } else {
+//     return `odd`
+// }
+// }
+
+// const num1 = evenOrOdd(13 % 2)
+// console.log(num1)
+
+
+// double functions
+const words = [
+    "The", "killing", "complex", "houses",
+    "married", "kittens", "and", "single",
+    "soldiers", "and", "their", "kleptomaniacal",
+    "families"
+]
+/*
+    Function to filter out `k` words
+    (Google 'javascript startswith' to get started)
+ */
+
+const filterOut = () => {
+
 }
+
+/*
+    Function to build a single string from the array
+    (Google 'javascript combine all items in array' to get started)
+ */
+
+
+/*
+    Invoke the filtering function and store its return value
+ */
+
+
+/*
+    Invoke the string building function, and pass the
+    return value of the previous function as an argument
+*/
+
+
+console.log()
